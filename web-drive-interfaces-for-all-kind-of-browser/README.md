f9fc420e3fe207dbf2c67e1e1c9bb116340e6ace
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-06-26 13:04:14 +0530

    Web drive interfaces

---
+++ web-drive-interfaces-for-all-kind-of-browser/README.md
@@ -0,0 +1,2 @@
+# Web driver interface for selecting a browser from the available browser list.
+ 
---
+++ web-drive-interfaces-for-all-kind-of-browser/Webdrive.java
@@ -0,0 +1,42 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Webdrive {
+	public static String browser = "chrome";
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (browser.equals("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+
+			driver = new FirefoxDriver();
+
+		} else if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+
+			driver = new ChromeDriver();
+
+		}else if(browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+
+		}
+
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div[1]/div/form/input")).click();
+		driver.close();
+
+	}
+
+}
