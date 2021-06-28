b7ab40f0f8f0dc370c948134303df2f6497e3f65
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-06-28 12:15:29 +0530

    Alert Handling

---
+++ alert-handling/Alert.java
@@ -0,0 +1,37 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Alert {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\akhil\\Downloads\\browser driver\\chromedriver.exe");
+		
+		WebDriverManager.chromedriver().setup();
+	
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert");
+		driver.manage().window().maximize();
+		driver.switchTo().frame(0);		           
+		
+	
+		driver.findElement(By.xpath("/html/body/button")).click();
+		String msg=driver.switchTo().alert().getText();
+		System.out.println(msg);
+		driver.switchTo().alert().accept();                              //handling alert
+		
+		Thread.sleep(500);
+		
+		
+		
+		
+
+	}
+
+}
