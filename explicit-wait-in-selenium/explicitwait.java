6685d0858f5f1f031706021be44d859305dcc38b
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-12-22 10:54:11 +0530

    explicit-wait

---
+++ explicit-wait-in-selenium/explicitwait.java
@@ -0,0 +1,27 @@
+package ui;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class explicitwait {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.ebay.com/");	  			
+		WebElement element= driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[3]/a"));
+		Actions action = new Actions(driver);
+		action.moveToElement(element).perform();
+		WebDriverWait explicitwait = new WebDriverWait(driver,10);
+		explicitwait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\\\"mainContent\\\"]/div[1]/ul/li[3]/div[2]/div[1]/nav[2]/ul/li[7]/a"))).click();
+	}
+
+}
