9b56712f7a1868294bdde28a80dc3656d7255957
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-12-27 15:56:55 +0530

    read-write-in-file

---
+++ read-write-in-file/readwrite.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class readwrite {
+
+	public static void main(String[] args) throws IOException {
+		Properties properties = new Properties();
+		FileInputStream inputstream = new FileInputStream("C:\\Users\\lenovo\\eclipse-workspace\\rw\\src\\test\\resources\\properties\\testdata.properties");
+	    properties.load(inputstream);
+	    String browserinfo= properties.getProperty("browser");
+	    System.out.println(browserinfo);
+	    System.out.println(properties.getProperty("url"));
+	}
+
+}
